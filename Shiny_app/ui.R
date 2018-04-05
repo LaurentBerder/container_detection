beb8b986d43302fc0a814cb1cca27f70aec2d7d3
Laurent Berder <laurent.berder@edu.dsti.institute>
2018-04-05 10:39:34 +0200

    Add files via upload

---
+++ Shiny_app/ui.R
@@ -0,0 +1,58 @@
+library("shiny")
+library("markdown")
+
+shinyUI(fluidPage(
+
+  title = "Detecting containers: counting containers in images",
+  theme = "bootstrap.min.css",
+
+  # css hacks for responsive images
+  tags$head(tags$style(
+    type="text/css",
+    "#original_image img {max-width: 100%; width: 100%; height: auto}"
+  )),
+
+  tags$head(tags$style(
+    type="text/css",
+    "#output_image img {max-width: 100%; width: 100%; height: auto}"
+  )),
+
+  titlePanel(title=div(img(src="logo.jpg"), "Container Recognition Artificial Neuralnetwork GUI (CRANG)")),
+
+  tags$hr(),
+
+  fluidRow(
+    column(
+      width = 6,
+      h4("Upload a picture"),
+      tags$hr(),
+      fileInput("file1", "Upload a PNG or JPEG image:")
+    )
+  ),
+
+  tags$hr(),
+
+  fluidRow(
+    column(
+      width = 6,
+      h4("Original picture"),
+      tags$hr(),
+      imageOutput("original_image", height = "auto")),
+    column(
+      width = 6,
+      h4("Detection"),
+      tags$hr(),
+      imageOutput("output_image", height = "auto")
+    )
+  ),
+
+  tags$hr(),
+
+  fluidRow(
+    column(
+      width = 12,
+      includeMarkdown("footer.md")
+    )
+  )
+
+))
