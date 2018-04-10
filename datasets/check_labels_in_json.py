ecc1febcc56ca47e13c74c514dd3db093ed89251
Laurent Berder <laurent.berder@edu.dsti.institute>
2018-04-10 08:40:40 +0200

    Update check_labels_in_json.py

--- datasets/check_labels_in_json.py
+++ datasets/check_labels_in_json.py
@@ -1,13 +1,19 @@
 import os
 import json
+import argparse
 
-directory = os.chdir("G:/My Drive/CMA-CGM/Reconnaissance_Container/Datasets/Data2/image_segmentation-Copy/")
-list = os.listdir(directory)
+ 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', help='The folder to scan')
+    args = parser.parse_args()
+    
+    directory = os.chdir(args.directory)
+    list = os.listdir(directory)
 
-for l in list:
-    if 'json' in l:
-        data = json.loads(open(l).read()).get('labels')
-        labels = [d.get('label_class') for d in data]
-        if None in labels:
-            print(l)
-            
+    for l in list:
+        if 'json' in l:
+            data = json.loads(open(l).read()).get('labels')
+            labels = [d.get('label_class') for d in data]
+            if None in labels:
+                print(l)
